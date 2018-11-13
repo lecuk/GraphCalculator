2e9b17bc9ef9156472b8d3a15d3892d95b46207c
lexa <voldemar11232@mail.com>
2018-11-13 15:19:41 +0200

    fixed resource file

--- GraphCalculator/Properties/Resources.Designer.cs
+++ GraphCalculator/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace GraphCalculator.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap texture {
-            get {
-                object obj = ResourceManager.GetObject("texture", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
